fbfbddcbfdf2ad6929be428787dc26a51960488a
Hanzhou Tang <hanzhoutang@gmail.com>
2017-11-15 23:48:35 -0600

    init

---
+++ joyfun/lib/joyfun.ex
@@ -0,0 +1,18 @@
+defmodule Joyfun do
+  @moduledoc """
+  Documentation for Joyfun.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Joyfun.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
